876ba9b1c302ee4d5e0e4218ca2cda3391446004
setfrank11 <105731465+setfrank11@users.noreply.github.com>
2023-09-27 20:03:24 -0500

    agregando modificacion de archivo js, queda reponsivo y el display activado de elemento

---
+++ main.js
@@ -0,0 +1,6 @@
+const shareBtn = document.querySelector('.card__content__button');
+const socialIcons = document.querySelector('.card__content__social-icons');
+
+shareBtn.addEventListener('click', ()=>{
+    socialIcons.classList.toggle('show');
+});
